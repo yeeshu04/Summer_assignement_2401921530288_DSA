bc9369ad9fd6fc5fe794c457134ce52c6903c470
yeeshu04 <yeeshumathur0404@gmail.com>
2026-07-04 10:24:27 +0530

    Add files via upload

---
+++ WEEK-2/DAY-3/subsequence.java
@@ -0,0 +1,53 @@
+package LEETCODE;
+
+class subsequence {
+
+    public boolean isSubsequence(String s, String t) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
+
+
+
+
+
+// public class subsequence {
+
+//     public static boolean isSubsequence(String s, String t) {
+
+//         int i = 0;
+//         int j = 0;
+
+//         while (i < s.length() && j < t.length()) {
+
+//             if (s.charAt(i) == t.charAt(j)) {
+//                 i++;
+//             }
+
+//             j++;
+//         }
+
+//         return i == s.length();
+//     }
+
+//     public static void main(String[] args) {
+
+//         String s = "abc";
+//         String t = "ahbgdc";
+
+//         System.out.println(isSubsequence(s, t));
+//     }
+// }
