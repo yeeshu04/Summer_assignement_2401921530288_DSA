b6f947a681b0cda4a5013ba3f9aba101e29f017e
yeeshu04 <yeeshumathur0404@gmail.com>
2026-07-20 22:47:31 +0530

    Add files via upload

---
+++ WEEK-4/DAY-1/SameTree.java
@@ -0,0 +1,103 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *
+ *     TreeNode() {}
+ *
+ *     TreeNode(int val) {
+ *         this.val = val;
+ *     }
+ *
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left)
+                && isSameTree(p.right, q.right);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class TreeNode {
+
+//     int val;
+//     TreeNode left;
+//     TreeNode right;
+
+//     TreeNode(int val) {
+//         this.val = val;
+//     }
+// }
+
+// public class SameTree {
+
+//     public static boolean isSameTree(TreeNode p, TreeNode q) {
+
+//         if (p == null && q == null) {
+//             return true;
+//         }
+
+//         if (p == null || q == null) {
+//             return false;
+//         }
+
+//         if (p.val != q.val) {
+//             return false;
+//         }
+
+//         return isSameTree(p.left, q.left)
+//                 && isSameTree(p.right, q.right);
+//     }
+
+//     public static void main(String[] args) {
+
+//         TreeNode root1 = new TreeNode(1);
+//         root1.left = new TreeNode(2);
+//         root1.right = new TreeNode(3);
+
+//         TreeNode root2 = new TreeNode(1);
+//         root2.left = new TreeNode(2);
+//         root2.right = new TreeNode(3);
+
+//         System.out.println(isSameTree(root1, root2));
+//     }
+// }
